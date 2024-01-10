877b7ceec6e78de1a97609eebe7a5000f46883ea
Irouane1 <itsirw@gmail.com>
2024-01-10 16:16:59 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,5 @@
--- script that lists all the tables of a database in my MySQL server.
+-- task: Write a script that lists all the tables of a database in your MySQL server.
+-- The database name will be passed as an argument of the mysql command.
 
 -- Show all tables in the specified database
 SHOW TABLES FROM `mysql`;
