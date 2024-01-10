e5c78fff4ca4ac510f80925c518ce5e3bb303882
Irouane1 <itsirw@gmail.com>
2024-01-10 16:18:45 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,21 @@
--- task: Write a script that lists all the tables of a database in your MySQL server.
--- The database name will be passed as an argument of the mysql command.
+-- Delete and create database hbtn_test_db_0 and create 3 tables
+DROP DATABASE IF EXISTS hbtn_test_db_0;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+USE hbtn_test_db_0;
+
+-- Create 3 tables
+CREATE TABLE IF NOT EXISTS holbteron_0 (
+    id INT
+);
+
+CREATE TABLE IF NOT EXISTS holbteron_1 (
+    name VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS holbteron_2 (
+    id INT,
+    name VARCHAR(256)
+);
 
 -- Show all tables in the specified database
-SHOW TABLES FROM `mysql`;
+SHOW TABLES;
