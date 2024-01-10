d0a2ac2d1d44a436bebfe063b258a578b6d59bd0
Irouane1 <itsirw@gmail.com>
2024-01-10 16:47:04 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -6,7 +6,7 @@ SET @db_name = DATABASE();
 
 SELECT
   CASE
-    WHEN COUNT(*) = 0 THEN 'No tables found in database ' + @db_name
+    WHEN COUNT(*) = 0 THEN CONCAT('No tables found in database ', @db_name)
     ELSE GROUP_CONCAT(table_name)
   END AS Tables_in_@db_name
 FROM tables
