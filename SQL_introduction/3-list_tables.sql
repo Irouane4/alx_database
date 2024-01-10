c0ccf6e5b28087b8116579163746b536b581d503
Irouane1 <itsirw@gmail.com>
2024-01-10 16:52:43 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -5,6 +5,6 @@ USE information_schema;
 SET @db_name = DATABASE();
 
 SELECT
-  IFNULL(GROUP_CONCAT(table_name), CONCAT('No tables found in database ', @db_name)) AS Tables_in_@db_name
+  COALESCE(GROUP_CONCAT(table_name), CONCAT('No tables found in database ', @db_name)) AS Tables_in_@db_name
 FROM tables
 WHERE table_schema = @db_name;
