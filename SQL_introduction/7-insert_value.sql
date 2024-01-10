53ee8c42320541e08a1f6be77d464f01215c7126
Irouane1 <itsirw@gmail.com>
2024-01-10 17:02:13 +0100

    commit

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Insert a new row into the table first_table
+
+-- Insert a new row with unique values, assuming 'id' is the primary key
+INSERT INTO first_table (id, name)
+VALUES (NULL, 'New Value');
