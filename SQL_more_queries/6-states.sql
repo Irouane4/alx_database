d4551d795fbfaac9b9c9ce147288a44c0da1106d
Irouane1 <itsirw@gmail.com>
2024-01-11 18:06:59 +0100

    commit

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,19 +1,14 @@
--- 6-states.sql
+-- This script creates the database hbtn_0d_usa and the table states in the database hbtn_0d_usa
 
--- Create database hbtn_0d_usa if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Use the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
--- Create table states if not exists
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
 
--- Insert values into states table
-INSERT INTO states (name) VALUES
-    ('California'),
-    ('Arizona'),
-    ('Texas');
+-- The following lines are used to populate the states table with example data
+
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");
