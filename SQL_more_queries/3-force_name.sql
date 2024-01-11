e353db957cd9c13a381101e0a732d1bbbcb3d2bc
Irouane1 <itsirw@gmail.com>
2024-01-11 17:36:33 +0100

    commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -2,7 +2,14 @@
 
 -- Create table force_name if not exists
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
 );
+
+-- Insert values into force_name table
+INSERT INTO force_name (name) VALUES
+    ('Holberton School'),
+    ('Python is cool'),
+    ('Holberton'),
+    ('School'),
+    ('C is fun');
