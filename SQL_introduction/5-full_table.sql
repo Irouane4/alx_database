b1f0304e03a459f9d8e14c4bb8971ae22284defb
Irouane1 <itsirw@gmail.com>
2024-01-11 18:19:59 +0100

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,7 @@
--- This script prints the full description of the table first_table from the database hbtn_0c_0
+-- 5-full_table.sql
 
-SELECT TABLE_NAME AS 'Table',
-       CONCAT('CREATE TABLE `', TABLE_NAME, '` (',\n GROUP_CONCAT(\n    CONCAT(' `', COLUMN_NAME, '` ', COLUMN_TYPE, IF(IS_NULLABLE = 'NO', ' NOT NULL', '')) SEPARATOR ', '),\n ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION, ';\n') AS 'Create Table'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
- AND TABLE_NAME = 'first_table'
-GROUP BY TABLE_NAME, ENGINE, TABLE_COLLATION;
+-- Use the hbtn_0c_0 database
+USE hbtn_0c_0;
+
+-- Show the full table description
+SHOW CREATE TABLE first_table;
