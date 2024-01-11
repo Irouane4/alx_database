9d64214415502f681316867d0702f8bad52df254
Irouane1 <itsirw@gmail.com>
2024-01-11 16:48:08 +0100

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,13 +9,13 @@ SELECT
             ' ',
             COLUMN_TYPE,
             IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
-            IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
+            IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
             IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''),
             ','
             ORDER BY ORDINAL_POSITION
         ),
         ' PRIMARY KEY (`id`)',
-        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+        ') ENGINE=InnoDB DEFAULT CHARSET=latin1;'
     ) AS create_table
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_test_db_5' AND TABLE_NAME = 'first_table';
