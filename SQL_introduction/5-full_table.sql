24fff3960c0a0fefca38948536d11e52ed569001
Irouane1 <itsirw@gmail.com>
2024-01-11 15:58:39 +0100

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,12 +1,6 @@
 -- Print the full description of the table first_table
 
 -- Use the information_schema to get the table definition
-SELECT
-  CONCAT(column_name, column_type,
-         IF(is_nullable = 'NO', 'NOT NULL', ''),
-         IF(column_default IS NOT NULL, CONCAT('DEFAULT ', column_default), ''),
-         IF(extra = 'auto_increment', 'AUTO_INCREMENT', ''),
-         IF(column_key = 'PRI', ', PRIMARY KEY (`', column_name, '`)','')
-  ) AS column_definition
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SELECT create_table
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_test_db_5' AND table_name = 'first_table';
