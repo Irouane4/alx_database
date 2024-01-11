94ffaa9225f482ef9262676075da8771b6d3567b
Irouane1 <itsirw@gmail.com>
2024-01-11 18:03:04 +0100

    commit

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- 9-cities_by_state_subquery.sql
+
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Select cities with state names using a subquery
+SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) as state_name
+FROM cities
+ORDER BY cities.id;
