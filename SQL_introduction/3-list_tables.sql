447cbda893ed5bb33b8192f3221bbf9da0e6123e
Irouane1 <itsirw@gmail.com>
2024-01-11 16:52:48 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,16 @@
--- List all tables in the specified database
+#!/bin/bash
 
--- Replace 'your_database_name' with the actual name of your database
-USE your_database_name;
+# Define the database name as an argument
+DATABASE_NAME="$1"
 
--- Show all tables in the specified database
+# Connect to the MySQL server
+mysql -hlocalhost -uroot -p
+
+# Switch to the specified database
+USE $DATABASE_NAME
+
+# List all tables in the database
 SHOW TABLES;
+
+# Exit the MySQL server
+exit 0
