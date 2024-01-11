2375aa0eeb756774c6746c4581f7c0f7b52829f6
Irouane1 <itsirw@gmail.com>
2024-01-11 17:27:40 +0100

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,4 @@
--- 0-privileges.sql
+-- Show grants for MySQL users user_0d_1 and user_0d_2 on localhost
 
--- Create user user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Grant all privileges to user_0d_1 if not already granted
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to user_0d_2 if not already granted
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
