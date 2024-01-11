b3ff86cd059c2b7fbce48ca48dcecb693ba857c3
Irouane1 <itsirw@gmail.com>
2024-01-11 16:25:50 +0100

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- Print the full description of the table first_table
 
 -- Use the information_schema to get the table definition
-SELECT table_definition
+SELECT CONCAT('first_table ', create_statement)
 FROM information_schema.tables
 WHERE table_schema = 'hbtn_test_db_5' AND table_name = 'first_table';
