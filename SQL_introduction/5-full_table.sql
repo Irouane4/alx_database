a44e41e2d79362de92410d0b36462f928abe7c6d
Irouane1 <itsirw@gmail.com>
2024-01-11 16:29:16 +0100

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,9 @@
--- Print the full description of the table first_table
+-- Script to print the full description of the table first_table from the database hbtn_0c_0
 
--- Use the information_schema to get the table definition
-SELECT CONCAT('first_table ', create_statement)
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_test_db_5' AND table_name = 'first_table';
+USE hbtn_0c_0; -- Select the specified database
+
+-- Query to retrieve the table structure without using DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
+
