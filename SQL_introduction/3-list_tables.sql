313fb4bdfe5423340b23b7e4aa461c138c5780da
Irouane1 <itsirw@gmail.com>
2024-01-11 16:57:18 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,14 +1,11 @@
+
 -- Define the database name as an argument
 DATABASE_NAME="$1"
 
--- Connect to the MySQL server
-mysql -hlocalhost -uroot -p
-
--- Switch to the specified database
-USE $DATABASE_NAME
+-- Connect to the MySQL server and switch to the specified database
+mysql -hlocalhost -uroot -p $DATABASE_NAME << EOF
 
 -- List all tables in the database
 SHOW TABLES;
 
--- Exit the MySQL server
-exit 0
+EOF
