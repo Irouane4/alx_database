f3c887af9529cf95a30f55cd4b23286df12d4c35
Irouane1 <itsirw@gmail.com>
2024-01-11 16:50:28 +0100

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,12 +1,7 @@
--- 3-list_tables.sql
--- Task: List all tables in the specified database.
+-- List all tables in the specified database
 
-USE INFORMATION_SCHEMA;
+-- Replace 'your_database_name' with the actual name of your database
+USE your_database_name;
 
-SET @DB_NAME = DATABASE();
-
--- Retrieve the list of tables or display a message if none found.
-SELECT
-  IFNULL(GROUP_CONCAT(TABLE_NAME), CONCAT('No tables found in database ', @DB_NAME)) AS Tables_in_@DB_NAME
-FROM TABLES
-WHERE TABLE_SCHEMA = @DB_NAME;
+-- Show all tables in the specified database
+SHOW TABLES;
